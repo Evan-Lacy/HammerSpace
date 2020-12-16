d3afcee17a0103e112961f48a970ae4d6a0b3129
Evan Lacy <lacyevan@yahoo.com>
2020-12-16 00:13:41 -0500

    Add project files.

---
+++ HammerSpace.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HammerSpace.WebMVC.MvcApplication" Language="C#" %>
---
+++ HammerSpace.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HammerSpace.WebMVC.Startup))]
+namespace HammerSpace.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
