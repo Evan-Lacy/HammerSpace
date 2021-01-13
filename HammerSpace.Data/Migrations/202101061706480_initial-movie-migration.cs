45d45b3c7dd5384afbdc983702fd329730a7bee8
Evan Lacy <lacyevan@yahoo.com>
2021-01-13 03:43:59 -0500

    Migration and CSS changes

--- HammerSpace.Data/Migrations/202101061706480_initial-movie-migration.cs
+++ HammerSpace.Data/Migrations/202101061706480_initial-movie-migration.cs
@@ -7,90 +7,90 @@ namespace HammerSpace.Data.Migrations
     {
         public override void Up()
         {
-            CreateTable(
-                "dbo.Movie",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        MovieTitle = c.String(nullable: false),
-                        MovieDescription = c.String(nullable: false),
-                        MovieRunTime = c.Double(nullable: false),
-                        Director = c.String(),
-                        MovieRating = c.Int(nullable: false),
-                        MovieGenre = c.Int(nullable: false),
-                        ReleaseYear = c.Int(),
-                    })
-                .PrimaryKey(t => t.Id);
+        //    CreateTable(
+        //        "dbo.Movie",
+        //        c => new
+        //            {
+        //                Id = c.Int(nullable: false, identity: true),
+        //                MovieTitle = c.String(nullable: false),
+        //                MovieDescription = c.String(nullable: false),
+        //                MovieRunTime = c.Double(nullable: false),
+        //                Director = c.String(),
+        //                MovieRating = c.Int(nullable: false),
+        //                MovieGenre = c.Int(nullable: false),
+        //                ReleaseYear = c.Int(),
+        //            })
+        //        .PrimaryKey(t => t.Id);
             
-            CreateTable(
-                "dbo.IdentityRole",
-                c => new
-                    {
-                        Id = c.String(nullable: false, maxLength: 128),
-                        Name = c.String(),
-                    })
-                .PrimaryKey(t => t.Id);
+        //    CreateTable(
+        //        "dbo.IdentityRole",
+        //        c => new
+        //            {
+        //                Id = c.String(nullable: false, maxLength: 128),
+        //                Name = c.String(),
+        //            })
+        //        .PrimaryKey(t => t.Id);
             
-            CreateTable(
-                "dbo.IdentityUserRole",
-                c => new
-                    {
-                        UserId = c.String(nullable: false, maxLength: 128),
-                        RoleId = c.String(),
-                        IdentityRole_Id = c.String(maxLength: 128),
-                        ApplicationUser_Id = c.String(maxLength: 128),
-                    })
-                .PrimaryKey(t => t.UserId)
-                .ForeignKey("dbo.IdentityRole", t => t.IdentityRole_Id)
-                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
-                .Index(t => t.IdentityRole_Id)
-                .Index(t => t.ApplicationUser_Id);
+        //    CreateTable(
+        //        "dbo.IdentityUserRole",
+        //        c => new
+        //            {
+        //                UserId = c.String(nullable: false, maxLength: 128),
+        //                RoleId = c.String(),
+        //                IdentityRole_Id = c.String(maxLength: 128),
+        //                ApplicationUser_Id = c.String(maxLength: 128),
+        //            })
+        //        .PrimaryKey(t => t.UserId)
+        //        .ForeignKey("dbo.IdentityRole", t => t.IdentityRole_Id)
+        //        .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+        //        .Index(t => t.IdentityRole_Id)
+        //        .Index(t => t.ApplicationUser_Id);
             
-            CreateTable(
-                "dbo.ApplicationUser",
-                c => new
-                    {
-                        Id = c.String(nullable: false, maxLength: 128),
-                        Email = c.String(),
-                        EmailConfirmed = c.Boolean(nullable: false),
-                        PasswordHash = c.String(),
-                        SecurityStamp = c.String(),
-                        PhoneNumber = c.String(),
-                        PhoneNumberConfirmed = c.Boolean(nullable: false),
-                        TwoFactorEnabled = c.Boolean(nullable: false),
-                        LockoutEndDateUtc = c.DateTime(),
-                        LockoutEnabled = c.Boolean(nullable: false),
-                        AccessFailedCount = c.Int(nullable: false),
-                        UserName = c.String(),
-                    })
-                .PrimaryKey(t => t.Id);
+        //    CreateTable(
+        //        "dbo.ApplicationUser",
+        //        c => new
+        //            {
+        //                Id = c.String(nullable: false, maxLength: 128),
+        //                Email = c.String(),
+        //                EmailConfirmed = c.Boolean(nullable: false),
+        //                PasswordHash = c.String(),
+        //                SecurityStamp = c.String(),
+        //                PhoneNumber = c.String(),
+        //                PhoneNumberConfirmed = c.Boolean(nullable: false),
+        //                TwoFactorEnabled = c.Boolean(nullable: false),
+        //                LockoutEndDateUtc = c.DateTime(),
+        //                LockoutEnabled = c.Boolean(nullable: false),
+        //                AccessFailedCount = c.Int(nullable: false),
+        //                UserName = c.String(),
+        //            })
+        //        .PrimaryKey(t => t.Id);
             
-            CreateTable(
-                "dbo.IdentityUserClaim",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        UserId = c.String(),
-                        ClaimType = c.String(),
-                        ClaimValue = c.String(),
-                        ApplicationUser_Id = c.String(maxLength: 128),
-                    })
-                .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
-                .Index(t => t.ApplicationUser_Id);
+        //    CreateTable(
+        //        "dbo.IdentityUserClaim",
+        //        c => new
+        //            {
+        //                Id = c.Int(nullable: false, identity: true),
+        //                UserId = c.String(),
+        //                ClaimType = c.String(),
+        //                ClaimValue = c.String(),
+        //                ApplicationUser_Id = c.String(maxLength: 128),
+        //            })
+        //        .PrimaryKey(t => t.Id)
+        //        .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+        //        .Index(t => t.ApplicationUser_Id);
             
-            CreateTable(
-                "dbo.IdentityUserLogin",
-                c => new
-                    {
-                        UserId = c.String(nullable: false, maxLength: 128),
-                        LoginProvider = c.String(),
-                        ProviderKey = c.String(),
-                        ApplicationUser_Id = c.String(maxLength: 128),
-                    })
-                .PrimaryKey(t => t.UserId)
-                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
-                .Index(t => t.ApplicationUser_Id);
+        //    CreateTable(
+        //        "dbo.IdentityUserLogin",
+        //        c => new
+        //            {
+        //                UserId = c.String(nullable: false, maxLength: 128),
+        //                LoginProvider = c.String(),
+        //                ProviderKey = c.String(),
+        //                ApplicationUser_Id = c.String(maxLength: 128),
+        //            })
+        //        .PrimaryKey(t => t.UserId)
+        //        .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+        //        .Index(t => t.ApplicationUser_Id);
             
         }
         
